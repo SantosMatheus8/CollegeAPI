00f4dfd075837cf21224d820424916c28b5f32d0
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-07-08 11:01:58 -0300

    Criando migration de Conteudos

---
+++ src/database/migrations/1657288749550-CreateConteudos.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateConteudos1657288749550 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "conteudos",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "disciplina_id",
+            type: "int",
+            isPrimary: true,
+          },
+          {
+            name: "descricao",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "link_conteudo",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ["disciplina_id"],
+            referencedTableName: "disciplinas",
+            referencedColumnNames: ["id"],
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("conteudos");
+  }
+}
