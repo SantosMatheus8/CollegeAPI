9c7e062f8c819759b90238ac09ecf44b26128c98
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-07-08 10:38:02 -0300

    Criando migration de estudantes e modelagem logica inicial

---
+++ src/database/migrations/1657287262451-CreateEstudantes.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateEstudantes1657287262451 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "estudantes",
+        columns: [
+          {
+            name: "estudante_id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "nome",
+            type: "varchar",
+            length: "45",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            length: "45",
+          },
+          {
+            name: "senha",
+            type: "varchar",
+            length: "18",
+          },
+          {
+            name: "data_nascimento",
+            type: "timestamp",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("estudantes");
+  }
+}
