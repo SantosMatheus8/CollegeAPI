19210099676f1e36270ebc5df373896f350b032f
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-07-11 09:26:53 -0300

    Criacao do controller UpdateDisciplina

---
+++ src/controllers/DisciplinaControllers/UpdateDisciplinaController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { UpdateDisciplinaService } from "../../services/DisciplinaServices/UpdateDisciplinaService";
+
+export class UpdateClassController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const id = Number(request.params.id);
+    const { nome, cargaHoraria, estudantes } = request.body;
+
+    const servive = new UpdateDisciplinaService();
+
+    const res = await servive.execute({ id, nome, cargaHoraria, estudantes });
+
+    if (res instanceof Error) {
+      return response.status(400).json(res.message);
+    }
+    return response.status(200).json(res);
+  }
+}
