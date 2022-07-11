24a074582d169a3f824a7631319aa8ac44b6dbe8
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-07-11 09:36:32 -0300

    Criacao do controller CreateConteudo

---
+++ src/controllers/ConteudoControllers/CreateConteudoController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { CreateConteudoService } from "../../services/ConteudoServices/CreateConteudoService";
+
+export class CreateConteudoController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { descricao, linkConteudo, disciplina } = request.body;
+
+    const servive = new CreateConteudoService();
+
+    const res = await servive.execute({
+      descricao,
+      linkConteudo,
+      disciplina,
+    });
+
+    if (res instanceof Error) {
+      return response.status(400).json(res.message);
+    }
+    return response.status(200).json(res);
+  }
+}
